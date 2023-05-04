8e211322135fc08cfc267d4d4f12d41a00ab59ee
SeokEui Hong <hongs7701@naver.com>
2023-05-05 02:19:36 +0900

    연습문제 8번 풀이 9번 미완성

--- chapter06_7 연습문제.cpp
+++ chapter06_7 연습문제.cpp
@@ -38,4 +38,112 @@ node b = 3 / null
 
 7.다음과 같은 연결 리스트에서 아래와 같은 코드를 실행한다고 하자, 실행이 끝난 후에 포인터 p가 가리키는 노드는 어떤 노드인가?
 답 : NULL이다. 마지막 노드가 가리키는 링크는 NULL인데, 해당 for 반복문은 마지막까지 이동시키기 때문이다.
+
+8.덱은 삽입과 삭제가 양끝에서 임의로 수행되는 자료 구조이다. 다음 그림과 같이단순연결리스트로 덱을 구현한다고 할 때 O(1) 시간
+내에 수행할 수 없는 연산은? 
+풀이 
+(1) insertFirst  : Node x->data = value / Node x->link = first / first = Node x -> 무조건 3개 연산만
+(3) deletFirst : removed = first / head = Node x -> link / free(removed) -> 무조건 3개 연산만 가능
+답 
+(4) deleteLast 
+deleteLast의 경우 마지막 노드 이전의 노드를 알아야 하는데 이를 위해서는 단순 연결 리스트에서는 head부터 for반복문으로 이동해야 한다.
 */ 
+//9.다음과 같이 단순 연결 리스트에 사용자가 입력하는 값을 저장했다가 출력하는 프로그램을 작성하라.
+#include <stdio.h> 
+#include <stdlib.h>
+#include <string.h>
+
+typedef int element;
+typedef struct ListNode{
+	element data;
+	struct ListNode *link;
+} ListNode;
+
+void error(char *message)
+{
+	fprintf(stderr, "%s\n", message);
+	exit(1);
+}
+
+ListNode* insert_first(ListNode *head, element value)
+{
+	ListNode *p = (ListNode *)malloc(sizeof(ListNode));
+	p->data = value;
+	p->link = head; //먼저 p를 완성시켜주고
+	head = p; //link 자체인 head == 다음 노드 자리. 이곳에  p를 넣어준다.
+	
+	return head; 
+}
+
+ListNode* insert(ListNode *head, ListNode *pre, element value)
+{
+	ListNode *p = (ListNode *)malloc(sizeof(ListNode));
+	p->data = value;
+	p->link = pre->link;
+	pre->link = p; //(3)p를 완성시켜주고 위치 집어넣어준다. 
+	
+	return head; 
+}
+
+ListNode* delete_first(ListNode *head)
+{
+	if(head==NULL)
+	{
+		return NULL; 
+	}
+	else
+	{
+	ListNode *removed; //데이터를 새로 주는 것이 아니기 때문에 동적 메모리 할당을 할 필요가 없다.
+	removed = head; //removed 값을 알기 위해서는 removed가 뭔지 알기 알아야 함. 이를  위해서 link 그자체인 head == 첫 노드 
+	head = removed->link;
+	free(removed);
+	
+	return head;
+	}
+}
+
+ListNode* delete_delete(ListNode *head, ListNode *pre)
+{
+	ListNode *removed;
+	removed = pre->link; //링크는 노드 그 자체이니깐 removed가 뭔지 알려면 pre->link를 넣어줘야함. 
+	pre->link = removed->link;
+	free(removed);
+	
+	return head; 
+}
+
+void print_list(ListNode *head)
+{
+	for(ListNode *p = head; p != NULL ; p = p->link)
+	{
+		printf("%d->",p->data);
+	}
+	printf("NULL\n"); //마지막이란 걸 알리기 위해	
+} 
+
+int main(void)
+{
+	ListNode *head = NULL; //head 생성 처음에는 비어있는 상태로 설정
+	int n;
+	printf("노드의 개수 : ");
+	scanf("%d",&n);
+	ListNode *location = head;
+
+	for(int i = 0; i < n; i++)
+	{
+		ListNode *node;
+		element value;
+		printf("노드 #%d의 데이터 : ", i+1);
+		scanf("%d", &value);
+		node->data = value;
+		head = node;
+		node->link = location->link;
+		location->link = node->link;
+	}
+
+	print_list(head);
+	
+	return 0;	
+} 
+	
+
